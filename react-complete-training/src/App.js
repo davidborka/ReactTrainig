5da9b0c9b89c4e7153dfd594f1f60ca0e32e3908
davidborka <david0borka@gmail.com>
2019-09-25 17:43:41 +0200

    UseUseStetHookForManipulatingStateInFunctionComponents

--- react-complete-training/src/App.js
+++ react-complete-training/src/App.js
@@ -1,39 +1,73 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person'
-
-class App extends Component{
-  state = {
+/*
+he reason for this being that in the Rules of Hooks ESLint plugin, there is a check to see if a component or function name is valid: Checks if the node is a React component name. 
+React component names must always start with a non-lowercase letter.
+*/
+const App = props =>{
+ const [personsState, setPersonsState] =  useState({
     persons: [
       {name: 'David', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age:26}
     ],
     otherState: 'some other value'
-  }
+  });
 
-  switchNameHandler = ()=>{
-  // console.log('Was clicked!');
-  // DON'T USE THIS!! : this.state.persons[0].name = "Maximilian";
-  this.setState({persons: [
-    {name: 'David', age: 25},
-    {name: 'Maximilian', age: 29},
-    {name: 'Stephanie', age:30}
-  ]})
-  }
+const switchNameHandler = ()=>{// console.log('Was clicked!');
+// DON'T USE THIS!! : this.state.persons[0].name = "Maximilian";
+setPersonsState({persons: [
+  {name: 'David', age: 25},
+  {name: 'Maximilian', age: 29},
+  {name: 'Stephanie', age:30}
+]})};
 
-  render(){
-    return(
+  return(
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is a paragraph from the same root element.</p>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: racing</Person>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age= {personsState.persons[0].age}/>
+      <Person name={personsState.persons[1].name} age= {personsState.persons[1].age}>My Hobbies: racing</Person>
+      <Person name= {personsState.persons[2].name} age= {personsState.persons[2].age}/>
     </div>
     );
-  }
 }
 
+
 export default App;
+
+// class App extends Component{
+  // state = {
+  //   persons: [
+  //     {name: 'David', age: 28},
+  //     {name: 'Manu', age: 29},
+  //     {name: 'Stephanie', age:26}
+  //   ],
+  //   otherState: 'some other value'
+  // }
+  
+//   switchNameHandler = ()=>{
+  // console.log('Was clicked!');
+  // DON'T USE THIS!! : this.state.persons[0].name = "Maximilian";
+  // this.setState({persons: [
+  //   {name: 'David', age: 25},
+  //   {name: 'Maximilian', age: 29},
+  //   {name: 'Stephanie', age:30}
+  // ]})
+//   }
+
+//   render(){
+//     return(
+//       <div className="App">
+//         <h1>Hi, I'm a React App</h1>
+//         <p>This is a paragraph from the same root element.</p>
+//         <button onClick={this.switchNameHandler}>Switch Name</button>
+//         <Person name= {personsState.persons[0].name} age= {personsState.persons[0].age}/>
+//         <Person name= {personsState.persons[1].name} age= {personsState.persons[1].age}>My Hobbies: racing</Person>
+//         <Person name= {personsState.persons[2].name} age= {personsState.persons[2].age}/>
+//       </div>
+//       );
+//   }
+// }
