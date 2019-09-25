fc179035f58da094a14372a5b4f7d512a0338e75
davidborka <david0borka@gmail.com>
2019-09-25 17:02:09 +0200

    UndestandingAndUsingState

--- react-complete-training/src/App.js
+++ react-complete-training/src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component{
+  state = {
+    persons: [
+      {name: 'David', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age:26}
+    ]
+  }
   render(){
     return(
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is a paragraph from the same root element.</p>
-      <Person name="David" age="5"/>
-      <Person name="Josh" age="40">My Hobbies: racing</Person>
-      <Person name="Chriss" age="50"/>
+      <button>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: racing</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     </div>
     );
   }
